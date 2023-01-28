434572b1dac746796613cde8b0e221bb85e16a78
Roy Stewart <roythomasstewart@gmail.com>
2023-01-28 14:41:24 -0500

    Refactored the main program

--- src/main.py
+++ src/main.py
@@ -1,16 +1,31 @@
 import sys
+from src.plugins import load_plugins
 
 def main():
-    match sys.argv[1]:
+    first_argument = sys.argv[1]
+    if (is_program_command(first_argument)):
+        run_command(first_argument)
+    else:
+        run_script()
+
+def is_program_command(value) -> bool:
+    pass
+
+def run_command(command):
+    match(command):
         case "--list":
             print("available scripts")
         case "--help":
             print("help text")
         case _:
-            # load plugins
-            # find script
-            # run script
-            print("Hello World!")
+            # throw error for unrecognized command
+            pass
+
+def run_script():
+    load_plugins()
+    # find script
+    # run script
+    print("Hello World!")
 
 if __name__ == "__main__":
     main()

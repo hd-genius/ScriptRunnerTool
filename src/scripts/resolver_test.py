4d4ab24e76d26ddc84f5388257b4b98067a51aa8
Roy Stewart <roythomasstewart@gmail.com>
2023-04-17 22:52:04 -0400

    Fix unit tests import

--- src/scripts/resolver_test.py
+++ src/scripts/resolver_test.py
@@ -4,7 +4,7 @@ from pathlib import Path
 import os
 from pyfakefs.fake_filesystem_unittest import TestCase
 from parameterized import parameterized
-from src.resolver import find_script_with_name, InvalidScriptNameError, ConflictingScriptNamesError
+from scripts.resolver import find_script_with_name, InvalidScriptNameError, ConflictingScriptNamesError
 
 
 def using_environment(variables):

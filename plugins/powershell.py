d4c1e5d2abbd3d5359845a242d405aeacaa169cf
Roy Stewart <roythomasstewart@gmail.com>
2023-04-18 12:22:01 -0400

    Add powershell plugin

---
+++ plugins/powershell.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+from api import register_handler, ScriptHandler
+from script import Script
+
+
+@register_handler
+class PowershellHandler(ScriptHandler):
+    def can_handle(file: Path) -> bool:
+        return file.suffix is ".ps1"
+
+    def create_script_for(file: Path) -> Script:
+        return PowerShellScript(file)
+
+
+class PowerShellScript(Script):
+    name = ""
+    path = ""
+    def __init__(self, file: Path) -> None:
+        super().__init__()
+        self.name = file.name
+        self.path = file
+
+    def execute():
+        pass

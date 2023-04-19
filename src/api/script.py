3d6fb5f4188276de35a5974744edfe35d9c67627
Roy Stewart <roythomasstewart@gmail.com>
2023-04-18 22:41:57 -0400

    Add more doc comments to api classes

--- src/api/script.py
+++ src/api/script.py
@@ -2,14 +2,15 @@ from abc import ABC, abstractmethod
 from pathlib import Path
 
 class Script(ABC):
+    """The base class representing a script that can be executed"""
     name: str
     path: Path
     
     def __init__(self, file: Path) -> None:
-        super().__init__()
         self.name = file.stem
         self.path = file
 
     @abstractmethod
-    def execute():
+    def execute(self):
+        """Runs the script file that this object represents"""
         pass
--- src/api/script_handler.py
+++ src/api/script_handler.py
@@ -7,8 +7,10 @@ class ScriptHandler(ABC):
     """The abstract base class for script handler plugins."""
     @abstractmethod
     def can_handle(file: Path) -> bool:
+        """A method that determines if the ScriptHandler can handle a file"""
         pass
 
     @abstractmethod
     def create_script_for(file: Path) -> Script:
+        """Creates a Script object representation of the given file"""
         pass
